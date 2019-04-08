debb978922abf0776f320b00041692b05e51b020
Zeqiang-Lai <916323301@qq.com>
2019-04-08 17:23:52 +0800

    change to use another io api

--- chapter 3/lab2/c++/src/readConfigFile.h
+++ chapter 3/lab2/c++/src/readConfigFile.h
@@ -10,11 +10,9 @@ string readConfigFile(const char * cfgfilepath, const string & key, string & val
     {
         cout<<"can not open cfg file!"<<endl;
     }
-    char tmp[1000];
-    while(!cfgFile.eof())//循环读取每一行
+    string line;
+    while(getline(cfgFile, line))//循环读取每一行
     {
-        cfgFile.getline(tmp,1000);//每行读取前1000个字符，1000个应该足够了
-        string line(tmp);
         size_t pos = line.find('=');//找到每行的“=”号位置，之前是key之后是value
         if(pos==string::npos) return NULL;
         string tmpKey = line.substr(0,pos);//取=号之前
