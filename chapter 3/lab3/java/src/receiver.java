5ef201b5680a47d182e1ac6777109ba96f131d67
刘世界 <905328134@qq.com>
2019-04-11 16:42:55 +0800

    java 实现实验三（缺少crc）

--- chapter 3/lab3/java/src/receiver.java
+++ chapter 3/lab3/java/src/receiver.java
@@ -9,9 +9,12 @@ import java.net.SocketAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
-public class receiver {
-    final private static String TAG = "SocketUdp: ";
+public class Receiver {
+    final private static String TAG = "SocketUdp: ";//用来输出错误信息，不用管
     private static final int PORT= 8888;//监听和发送的端口
+
+    
+
 
     public static void main(String args[]) {
 
@@ -19,30 +22,47 @@ public class receiver {
         DatagramPacket datapacket_send = null;
         DatagramPacket datapacket_receive= null;
         InetSocketAddress address = null;
+        int frame_expected=0;
+        //死循环不会退出
         while(true) {
+
             try {
                 //定义接收的socket客户端
                 address = new InetSocketAddress(InetAddress.getLocalHost(), PORT);
                 socket = new DatagramSocket(address);
 
 
-                byte buf_receive[] = new byte[1024];
+                byte [] buf_receive = new byte[1024];
                 datapacket_receive = new DatagramPacket(buf_receive, buf_receive.length);
-                System.out.println("等待接收消息...");
+                System.out.println("等待接收:第"+frame_expected+"帧");
                 socket.receive(datapacket_receive);
                 InetAddress addr = datapacket_receive.getAddress();
                 int port = datapacket_receive.getPort();
-                System.out.println("消息内容: " + new String(buf_receive));
-                System.out.println("消息发送方 " + addr + ":" + port);
+                //这句太关键了，写尼玛傻逼博客的脑残非得用下面这句，搞得我调试了一天
+                //printf(new String(buf_receive))
+                //这才是正确的写法好吧
+                System.out.println("消息内容: " + new String(datapacket_receive.getData(),0,datapacket_receive.getLength()));
+                //System.out.println("消息发送方 " + addr + ":" + port);
 
                 SocketAddress toAddress = datapacket_receive.getSocketAddress();
                 //如果crc校验通过
-                String sendStr = "true";
-                byte [] buf_send = sendStr.getBytes();
-                datapacket_send = new DatagramPacket(buf_send, buf_send.length);
-                datapacket_send.setSocketAddress(toAddress);
-                socket.send(datapacket_send);
-                System.out.println("ack已经发送");
+                if(true) {
+                    String sendStr;
+                    if (frame_expected == 0)
+                        sendStr = "0";
+                    else
+                        sendStr = "1";
+                    byte[] buf_send = sendStr.getBytes();
+                    datapacket_send = new DatagramPacket(buf_send, buf_send.length);
+                    datapacket_send.setSocketAddress(toAddress);
+                    socket.send(datapacket_send);
+                    System.out.println("ack已经发送,确认已收到第" + frame_expected + "帧");
+                    frame_expected = 1 - frame_expected;
+                }
+                else
+                {
+                       System.out.println("收到的数据出错");
+                }
 
 
             } catch (UnknownHostException e) {
--- chapter 3/lab3/java/src/sender.java
+++ chapter 3/lab3/java/src/sender.java
@@ -7,24 +7,38 @@ import java.net.InetSocketAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
-public class sender {
-    final private static String TAG = "SocketUdp: ";
-    private static final int TIMEOUT = 50000;  //设置接收数据的超时时间
+public class Sender {
+    final private static String TAG = "SocketUdp: ";//输出错误信息用不用管
+    private static final int TIMEOUT = 5000;  //设置接收数据的超时时间
     private static final int PORT= 8888;//监听和发送的端口
-    private static final int FRAME_SIZE=10;//
+    private static final int FILTERERROR=10;//平均每十帧出错一次
+    private static final int FILTERLOST=10;//平均没十帧丢失一帧
+    public static int MyFilter()
+    {
+
+        double random_n1=Math.random();
+        double random_n2=Math.random();
+        //当随机数大于失败概率发送成功
+        if(random_n1>(1/FILTERLOST))
+        {
+            if(random_n2>(1/FILTERERROR))
+                return 1;
+            else
+                return 2;
+        }
+        else return 0;
+
+    }
     public static void main(String args[])
     {
         //从文件中读取一行
-        String message_to_send="test message 1";
-        //将读到的一行转化为二进制数字
-        byte[] message_byte_stream=message_to_send.getBytes();
-        //计算需要切分为多少帧
-        int frame_amount=message_byte_stream.length/FRAME_SIZE;
-        //定义一个计数器，判断是否计算完毕
-        int sended_amout=0;
+        String sendStr="test message 1";
+        //成功发送次数的计数器
+        int success_sended_amout=0;
+        //发送完所有数据后退出，success_sended_amount控制
         while(true)
         {
-
+            //发送成功后退出
             while(true)
             {
                 try {
@@ -37,37 +51,66 @@ public class sender {
                     DatagramPacket datapacket_receive = null;
                     //需要发送的地址，这里是本地发本地
                     InetSocketAddress toAddress = new InetSocketAddress(InetAddress.getLocalHost(), PORT);
-
-
-                    String sendStr = "I'm sender, this is the message for receiver.";
+                    //设置收发数据的存储区
                     byte buf_send[] = sendStr.getBytes();
                     byte buf_receive[]=new byte[1024];
+                    //配置发送包的内容和地址
                     datapacket_send = new DatagramPacket(buf_send, buf_send.length);
                     datapacket_send.setSocketAddress(toAddress);
-                    getSocket.send(datapacket_send);
-                    System.out.println("消息已经发送");
-
-
-
-                    System.out.println("等待接收ack...");
-                    datapacket_receive = new DatagramPacket(buf_receive, buf_receive.length);
-                    getSocket.receive(datapacket_receive);
-                    String ack_receive=new String(buf_receive);
-                    //如果接收ack成功
-                    if(ack_receive.equals("true"))
+                    //输出发送的帧号和下一帧的编号
+                    System.out.println("已发送：第"+(success_sended_amout)%2+"帧\n将发送：第"+(success_sended_amout+1)%2+"帧");
+                    //运行过滤程序，并输出发送情况
+                    int filter_result=MyFilter();
+                    if(filter_result==1)
                     {
-                        System.out.println("ack的内容: " + ack_receive);
+
+                        getSocket.send(datapacket_send);
+                        System.out.println("模拟发送成功");
+
+                    }
+                    else if(filter_result==2)
+                    {
+                        //改变发送的数据后再发送
+
+                        getSocket.send(datapacket_send);
+                        System.out.println("模拟传输出错");
+
+                    }
+                    else
+                    {
+                        System.out.println("模拟丢失");
+                    }
+
+                    //等待接收ack
+                    //配置接收包
+                    datapacket_receive = new DatagramPacket(buf_receive, buf_receive.length);
+                    //在时间TIMEOUT的范围内接收ack
+                    getSocket.receive(datapacket_receive);
+                    //将接收的ack从包中提取出来
+                    //存储ack_received中
+                    String ack_received=new String(datapacket_receive.getData(),0,datapacket_receive.getLength());
+                    String ack_expected;
+                    if(success_sended_amout%2==0)
+                        ack_expected="0";
+                    else
+                        ack_expected="1";
+                    //如果接收ack成功
+                    if(ack_received.equals(ack_expected))
+                    {
+                        System.out.println("ack的内容: " + ack_received);
+                        System.out.println("ack与期望一致，发送下一帧");
                         //更新一帧需要发送的信息
-                        sendStr="next_frame";
-                        datapacket_send.setData(sendStr.getBytes());
-                        sended_amout++;
+                        sendStr="test message "+(success_sended_amout+1);
+                        //datapacket_send.setData(sendStr.getBytes());
+                        //发送成功的计数器加一
+                        success_sended_amout++;
                         break;
                     }
                     //如果接收ack失败
                     else
-                        {
-                            System.out.println("ack与期望不符，重发当前帧");
-                            System.out.println("ack的内容: " + ack_receive);
+                    {
+                        System.out.println("ack的内容: " + ack_received);
+                        System.out.println("ack与期望不符，重发当前帧");
 
                     }
                     //关闭socket客户端
@@ -87,10 +130,13 @@ public class sender {
                 }
 
             }
-            if(sended_amout==1)
+            if(success_sended_amout==10)
             {
                 System.out.println("传输完毕");
+                break;
+
             }
+
         }
     }
 }
