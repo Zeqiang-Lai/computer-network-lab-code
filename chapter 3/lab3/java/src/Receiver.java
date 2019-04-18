487fb178ea9130175561b36babd0f8031eafc50c
刘世界 <905328134@qq.com>
2019-04-18 23:29:54 +0800

    最后一次

--- chapter 3/lab3/java/src/Receiver.java
+++ chapter 3/lab3/java/src/Receiver.java
@@ -12,7 +12,11 @@ import java.net.UnknownHostException;
 public class Receiver {
     final private static String TAG = "SocketUdp: ";//用来输出错误信息，不用管
     private static final int PORT= 8888;//监听和发送的端口
-    public static void main (String args[]) {
+
+    
+
+
+    public static void main(String args[]) {
 
         DatagramSocket socket = null;
         DatagramPacket datapacket_send = null;
@@ -30,38 +34,10 @@ public class Receiver {
 
                 byte [] buf_receive = new byte[1024];
                 datapacket_receive = new DatagramPacket(buf_receive, buf_receive.length);
-
-
-
-                //
-                //Sending ACK[number]
-                System.out.println("Frame expected: ["+frame_expected+"]");
+                System.out.println("等待接收:第"+frame_expected+"帧");
                 socket.receive(datapacket_receive);
                 InetAddress addr = datapacket_receive.getAddress();
                 int port = datapacket_receive.getPort();
-
-                //去掉crc位和flag位
-                byte[] buf_message=new byte[datapacket_receive.getLength()-3];
-                System.arraycopy(buf_receive,1,buf_message,0,datapacket_receive.getLength()-3);
-                byte[] buf_message_with_crc=new byte[datapacket_receive.getLength()-1];
-                System.arraycopy(buf_receive,1,buf_message_with_crc,0,datapacket_receive.getLength()-1);
-                byte[] buf_flag=new byte[1];
-                System.arraycopy(buf_receive,0,buf_flag,0,1);
-                String str_flag=new String(buf_flag);
-                //校验crc
-                System.out.println("Checking...");
-                String str_poly="10001000000100001";
-                String crc_result=crc.crc_check(new StringBuffer(BinaryChange.conver2HexStr(buf_message_with_crc)),new StringBuffer(str_poly));
-                //System.out.println("crc校验前："+BinaryChange.conver2HexStr(buf_message_with_crc) );
-                //System.out.println("crc校验结果是："+crc_result);
-
-
-
-
-
-
-
-                /*
                 //这句太关键了，写尼玛傻逼博客的脑残非得用下面这句，搞得我调试了一天
                 //printf(new String(buf_receive))
                 //这才是正确的写法好吧
@@ -87,34 +63,6 @@ public class Receiver {
                 {
                        System.out.println("收到的数据出错");
                 }
-                */
-
-                SocketAddress toAddress = datapacket_receive.getSocketAddress();
-                //如果收到的帧是想要的
-                if( Integer.parseInt(str_flag)==frame_expected) {
-                    int pos=crc_result.indexOf("1");
-                    if(pos==-1)
-                    {
-
-
-                        byte[] buf_send = str_flag.getBytes();
-                        datapacket_send = new DatagramPacket(buf_send, buf_send.length);
-                        datapacket_send.setSocketAddress(toAddress);
-                        socket.send(datapacket_send);
-                        System.out.println("Valid: Frame["+frame_expected+"]");
-                        frame_expected = 1 - frame_expected;
-                    }
-                    else
-                        System.out.println("Invalid: CRC = ["+crc_result+"]");
-
-                }
-                else
-                {
-                    System.out.println("\"InValid: Frame[\"+frame_expected+\"]\"");
-                }
-
-
-
 
 
             } catch (UnknownHostException e) {
